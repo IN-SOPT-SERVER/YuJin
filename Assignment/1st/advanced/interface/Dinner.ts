cc100a4d22624d2503f6c586aacac6f298ebadfb
yujindonut <bbb3631@naver.com>
2022-10-03 17:03:22 +0900

    [CHORE] 브랜치 수정

--- Assignment/1st/advanced/interface/Dinner.ts
+++
@@ -1,7 +0,0 @@
-import Member from "./Member";
-
-export default interface Dinner {
-    members: Member[];
-    shuffle: (array: Member[]) => Member[];
-    organize: (array: Member[]) => void;
-}
--- Assignment/1st/advanced/interface/Member.ts
+++
@@ -1,4 +0,0 @@
-export default interface Member {
-    name: string;
-    group: string;
-}
