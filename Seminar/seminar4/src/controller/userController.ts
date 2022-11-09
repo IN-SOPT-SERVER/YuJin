dc04c0fc2f86ab8c18485ce683c7bff5ae4af3f8
yujindonut <hyujin0825@gmail.com>
2022-11-10 00:42:14 +0900

    [FEAT]: 4차 세미나 - 기본과제
    4차 세미나 코드들을 다시 쳐보고 작성했습니다.

---
+++ Seminar/seminar4/src/controller/userController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { userService } from "../service";
+
+//* 유저 생성: 회원가입 부분
+const createUser = async (req: Request, res: Response) => {
+  //Request의 넘어오는 필드명이 변수명과 같을때 : 비구조화 할당!
+  const { userName, email, age } = req.body // DB의 필드명과 같게 설정함
+
+  if (!userName || !email || !age) {
+    return res.status(400).json({ status: 400, message: "유저 생성 실패" });
+  }
+
+  const data = await userService.createUser(userName, email, age);
+
+  if (!data) {
+    return res.status(400).json({ status: 400, message: "유저 생성 실패" });
+  }
+
+  return res.status(200).json({ status: 200, message: "유저 생성 성공", data });
+};
+
+//* 유저 전체 조회
+const getAllUser = async (req: Request, res: Response) => {
+  const data = await userService.getAllUser();
+  return res.status(200).json({ status: 200, message: "유저 전체 조회 성공", data });
+};
+
+//* 유저 정보 업데이트
+const updateUser = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const { userId } = req.params;
+
+  if (!name) return res.status(400).json({ status: 400, message: "유저 업데이트 실패" });
+
+  const updatedUser = await userService.updateUser(+userId, name);
+  return res.status(200).json({ status: 200, message: "유저 업데이트 성공", updatedUser });
+};
+
+//* 유저 삭제
+const deleteUser = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  await userService.deleteUser(+userId);
+  return res.status(200).json({ status: 200, message: "유저 삭제 성공" });
+};
+
+const getUserById = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  const data = await userService.getUserById(+userId);
+
+  if (!data) {
+    return res.status(404).json({ status: 404, message: "NOT_FOUND" });
+  }
+
+  return res.status(200).json({ status: 200, message: "유저 조회 성공", data });
+};
+
+const userController = {
+  createUser,
+  getAllUser,
+  updateUser,
+  deleteUser,
+  getUserById,
+};
+
+export default userController;
