e52a85bf9f8ca8e036186b2807f972adb0dfd6cd
yujindonut <hyujin0825@gmail.com>
2022-11-25 14:09:35 +0900

    [REFACTOR] 기존 코드 리팩토링
    기존 코드 리팩토링

--- Seminar/seminar4/src/controller/userController.ts
+++ Seminar/seminar4/src/controller/userController.ts
@@ -1,3 +1,4 @@
+import { MemberUpdateDTO } from './../interfaces/MemberUpdateDTO';
 import { Request, Response } from "express";
 import { userService } from "../service";
 import { MemberCreateDTO } from "../interfaces/MemberCreateDTO";
@@ -8,7 +9,6 @@ import jwtHandler from "../modules/jwtHandler";
 import { validationResult } from "express-validator";
 
 //* 유저 생성: 회원가입 부분
-
 const createUser = async (req: Request, res: Response) => {
   const error = validationResult(req);
   if (!error.isEmpty()) {
@@ -22,7 +22,6 @@ const createUser = async (req: Request, res: Response) => {
     return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.SIGNUP_FAIL));
   }
 
-  // ================== 여기 추가 ========================
   //? 아까 만든 jwtHandler 내 sign 함수를 이용해 accessToken 생성
   const accessToken = jwtHandler.sign(data.id);
 
@@ -34,44 +33,6 @@ const createUser = async (req: Request, res: Response) => {
 
   return res.status(sc.CREATED).send(success(sc.CREATED, rm.SIGNUP_SUCCESS, result));
 };
-
-//* 유저 전체 조회
-const getAllUser = async (req: Request, res: Response) => {
-  const data = await userService.getAllUser();
-  return res.status(200).json({ status: 200, message: "유저 전체 조회 성공", data });
-};
-
-//* 유저 정보 업데이트
-const updateUser = async (req: Request, res: Response) => {
-  const { name } = req.body;
-  const { userId } = req.params;
-
-  if (!name) return res.status(400).json({ status: 400, message: "유저 업데이트 실패" });
-
-  const updatedUser = await userService.updateUser(+userId, name);
-  return res.status(200).json({ status: 200, message: "유저 업데이트 성공", updatedUser });
-};
-
-//* 유저 삭제
-const deleteUser = async (req: Request, res: Response) => {
-  const { userId } = req.params;
-
-  await userService.deleteUser(+userId);
-  return res.status(200).json({ status: 200, message: "유저 삭제 성공" });
-};
-
-const getUserById = async (req: Request, res: Response) => {
-  const { userId } = req.params;
-
-  const data = await userService.getUserById(+userId);
-
-  if (!data) {
-    return res.status(404).json({ status: 404, message: "NOT_FOUND" });
-  }
-
-  return res.status(200).json({ status: 200, message: "유저 조회 성공", data });
-};
-
 
 //* 로그인
 const signInUser = async (req: Request, res: Response) => {
@@ -96,7 +57,57 @@ const signInUser = async (req: Request, res: Response) => {
       accessToken,
     };
 
-    res.status(sc.OK).send(success(sc.OK, rm.SIGNUP_SUCCESS, result));
+    res.status(sc.OK).send(success(sc.OK, rm.SIGNIN_SUCCESS, result));
+  } catch (e) {
+    //? 서버 내부에서 오류 발생
+    res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  }
+};
+
+//* 유저 전체 조회
+const getAllUser = async (req: Request, res: Response) => {
+  const data = await userService.getAllUser();
+  return res.status(sc.OK).send(success(sc.OK, rm.READ_ALL_USERS_SUCCESS, data));
+};
+
+//* 유저 조회
+const getUserById = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  const data = await userService.getUserById(+userId);
+
+  if (!userId) return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NOT_FOUND));
+
+  return res.status(sc.OK).send(success(sc.OK, rm.READ_USER_SUCCESS, data));
+};
+
+//* 유저 정보 업데이트
+const updateUser = async (req: Request, res: Response) => {
+
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+  }
+
+  // const token = req.headers.token;
+
+  const memberUpdateDTO: MemberUpdateDTO = req.body;
+  const { userId } = req.params;
+
+  try {
+    const updateUser = await userService.updateUser(+userId, memberUpdateDTO);
+
+    if (!updateUser) return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NOT_FOUND));
+    else if (updateUser === sc.UNAUTHORIZED)
+      return res.status(sc.UNAUTHORIZED).send(fail(sc.UNAUTHORIZED, rm.INVALID_PASSWORD));
+
+    // const accessToken = jwtHandler.verify(token);
+
+    const result = {
+      id: updateUser,
+    };
+
+    res.status(sc.OK).send(success(sc.OK, rm.UPDATE_USER_SUCCESS, result));
   } catch (e) {
     console.log(error);
     //? 서버 내부에서 오류 발생
@@ -104,6 +115,13 @@ const signInUser = async (req: Request, res: Response) => {
   }
 };
 
+//* 유저 삭제
+const deleteUser = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  await userService.deleteUser(+userId);
+  return res.status(sc.OK).send(success(sc.OK, rm.DELETE_USER_SUCCESS));
+};
 
 const userController = {
   createUser,
--- Seminar/seminar4/src/router/userRouter.ts
+++ Seminar/seminar4/src/router/userRouter.ts
@@ -7,14 +7,14 @@ const router: Router = Router();
 
 router.get("/:userId", auth, userController.getUserById);
 
-//* 유저 생성 - POST api/user
-// router.post('/', userController.createUser);
-
 //* 전체 유저 조회 - GET api/ user
 router.get('/', userController.getAllUser);
 
 //* 유저 정보 업데이트 - PATCH api/user/:userId
-router.patch('/:userId', userController.updateUser);
+router.patch('/:userId',
+    [body("name").notEmpty(), body("email").notEmpty(), body("password").isLength({ min: 6 })],
+    userController.updateUser
+);
 
 //* 유저 삭제 - DELETE api/user:userId
 router.delete('/:userId', userController.deleteUser);
